0bcc1b397767c01c955a6beae1b7eb6c506685f3
Irene Robb <irenerobb72@gmail.com>
2016-05-27 13:38:28 -0400

    asiudn

---
+++ app/app.js
@@ -0,0 +1,37 @@
+var jsonCircles = [
+  { "x_axis": 830, "y_axis": 60 "radius": 20, "color" : "green" },
+  { "x_axis": 850, "y_axis": 60 "radius": 20, "color" : "green" },
+  { "x_axis": 870, "y_axis": 60 "radius": 20, "color" : "green" },
+  { "x_axis": 890, "y_axis": 60 "radius": 20, "color" : "green" },
+  { "x_axis": 910, "y_axis": 60 "radius": 20, "color" : "green" },
+
+  { "x_axis": 830, "y_axis": 80 "radius": 20, "color" : "green" },
+  { "x_axis": 850, "y_axis": 80 "radius": 20, "color" : "green" },
+  { "x_axis": 870, "y_axis": 80 "radius": 20, "color" : "green" },
+  { "x_axis": 890, "y_axis": 80 "radius": 20, "color" : "green" },
+  { "x_axis": 910, "y_axis": 80 "radius": 20, "color" : "green" },
+
+  { "x_axis": 830, "y_axis": 100 "radius": 20, "color" : "green" },
+  { "x_axis": 850, "y_axis": 100 "radius": 20, "color" : "green" },
+  { "x_axis": 870, "y_axis": 100 "radius": 20, "color" : "green" },
+  { "x_axis": 890, "y_axis": 100 "radius": 20, "color" : "green" },
+  { "x_axis": 910, "y_axis": 100 "radius": 20, "color" : "green" },
+
+  { "x_axis": 830, "y_axis": 120 "radius": 20, "color" : "green" },
+  { "x_axis": 850, "y_axis": 120 "radius": 20, "color" : "green" },
+  { "x_axis": 870, "y_axis": 120 "radius": 20, "color" : "green" },
+  { "x_axis": 890, "y_axis": 120 "radius": 20, "color" : "green" },
+  { "x_axis": 910, "y_axis": 120 "radius": 20, "color" : "green" },
+
+  { "x_axis": 830, "y_axis": 140 "radius": 20, "color" : "green" },
+  { "x_axis": 850, "y_axis": 140 "radius": 20, "color" : "green" },
+  { "x_axis": 870, "y_axis": 140 "radius": 20, "color" : "green" },
+  { "x_axis": 890, "y_axis": 140 "radius": 20, "color" : "green" },
+  { "x_axis": 910, "y_axis": 140 "radius": 20, "color" : "green" },
+
+  { "x_axis": 830, "y_axis": 160 "radius": 20, "color" : "green" },
+  { "x_axis": 850, "y_axis": 160 "radius": 20, "color" : "green" },
+  { "x_axis": 870, "y_axis": 160 "radius": 20, "color" : "green" },
+  { "x_axis": 890, "y_axis": 160 "radius": 20, "color" : "green" },
+  { "x_axis": 910, "y_axis": 160 "radius": 20, "color" : "green" },
+]
